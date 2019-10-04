88e30c74d941168deacd2ecb5274e6e6d9ac1721
Anton Lapin <anton.lapin@jivygropup.com>
2019-10-04 13:39:32 +0300

    add test for PR1

--- src/translations/messages.js
+++ src/translations/messages.js
@@ -4,11 +4,13 @@ export default {
     'app.intro': 'Second paragraf',
     'app.adress': 'Our Store Adress',
     'app.adressStreet': 'Str: Marksyskaia 38, Moscow, Russia, 109147',
+    'app.newTest': 'Something',
   },
   ru: {
     'app.title': 'Главная страница',
     'app.intro':'второй параграф',
     'app.adress': 'Адресс нашего Ювелирного салона',
     'app.adressStreet': 'Марксистская ул., 38, Москва, Россия, 109147',
+    'app.newTest': 'Какой то текст',
   }
 }
